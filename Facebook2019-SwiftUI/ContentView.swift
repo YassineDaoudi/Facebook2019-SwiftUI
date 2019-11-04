9923ae2c3c1b70600123691251bf7869c4596c88
YassineDaoudi <yassine.daoudi@uir.ac.ma>
2019-11-04 14:55:43 +0100

    Initial Commit

---
+++ Facebook2019-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facebook2019-SwiftUI
+//
+//  Created by Findl on 11/4/19.
+//  Copyright © 2019 Yassine Daoudi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
